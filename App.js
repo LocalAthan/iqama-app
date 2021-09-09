ab83cf756e4399f3ee3919d108fb0e4dd6a2e94e
Yaser <yaser@ualberta.ca>
2021-09-09 15:47:47 -0600

    Show fajr prayer time info when mosque selected

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ import {
 import React, { useEffect, useState } from "react";
 import { API_URL } from "@env";
 import typography from "./styles/typography";
+import { TouchableHighlight } from "react-native-gesture-handler";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -24,11 +25,12 @@ export default function App() {
 
   const [isLoading, setLoading] = useState(true);
   const [mosques, setMosques] = useState([]);
+  const [currentMosque, setCurrentMosque] = useState();
+  const [prayerTimes, setPrayerTimes] = useState();
 
   const getMosques = async () => {
     try {
-      let response = await fetch(`${API_URL}/mosques`);
-      console.log();
+      const response = await fetch(`${API_URL}/mosques`);
       let json = await response.json();
       return json;
     } catch (error) {
@@ -36,15 +38,37 @@ export default function App() {
     }
   };
 
+  const getPrayerTimes = async (mosqueId) => {
+    try {
+      const response = await fetch(`${API_URL}/prayerTimes/${mosqueId}`);
+      let json = await response.json();
+      return json;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       const result = await getMosques();
       setMosques(result);
+      setCurrentMosque(result[0])
       setLoading(false);
     };
 
     fetchData();
   }, []);
+
+  useEffect(() => {
+    //TODO: fetch prayer times
+    const fetchData = async () => {
+      const result = await getPrayerTimes(currentMosque.id);
+      setPrayerTimes(result)
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [currentMosque]);
 
   return !fontsLoaded ? (
     <View style={[styles.center]}>
@@ -52,28 +76,109 @@ export default function App() {
     </View>
   ) : (
     <View style={styles.container}>
-      <View style={{ flex: 1 }} />
+      <View style={{ marginTop: 54, flex: 1 }}>
+        <View style={{ backgroundColor: "white" }}>
+          <View
+            style={{
+              marginVertical: 12,
+              marginHorizontal: 24,
+            }}
+          >
+            <Text
+              style={{
+                marginVertical: 12,
+                fontFamily: typography.FONT_FAMILY_REGULAR,
+                fontSize: typography.FONT_SIZE_LARGE,
+              }}
+            >
+              {currentMosque?.name}
+            </Text>
+            <View style={{ flexDirection: "row" }}>
+              <Text
+                style={{
+                  flex: 1,
+                  marginVertical: 12,
+                  fontFamily: typography.FONT_FAMILY_REGULAR,
+                  fontSize: typography.FONT_SIZE_LARGE,
+                }}
+              >
+                Athan:
+              </Text>
+              <Text
+                style={{
+                  flex: 3,
+                  marginVertical: 12,
+                  fontFamily: typography.FONT_FAMILY_BOLD,
+                  fontSize: typography.FONT_SIZE_LARGE,
+                }}
+              >
+                {prayerTimes?.fajr.athan}
+              </Text>
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              <Text
+                style={{
+                  flex: 1,
+                  marginVertical: 12,
+                  fontFamily: typography.FONT_FAMILY_REGULAR,
+                  fontSize: typography.FONT_SIZE_LARGE,
+                }}
+              >
+                Iqama:
+              </Text>
+              <Text
+                style={{
+                  flex: 3,
+                  marginVertical: 12,
+                  fontFamily: typography.FONT_FAMILY_BOLD,
+                  fontSize: typography.FONT_SIZE_LARGE,
+                }}
+              >
+                {prayerTimes?.fajr.iqama}
+              </Text>
+            </View>
+          </View>
+        </View>
+      </View>
       <View style={styles.mosqueList}>
         <FlatList
           keyExtractor={(item) => item.id.toString()}
           data={mosques}
           renderItem={({ item }) => (
-            <View style={styles.mosqueItem}>
-              <View style={styles.imageContainer}>
-                <Image
-                  style={styles.mosqueIcon}
-                  source={{ uri: item.imageUrl ? item.imageUrl : undefined }}
-                  defaultSource={{
-                    uri: "/Users/yaser/Projects/iqama-app/assets/mosque_placeholder.png",
-                  }}
-                />
-              </View>
-              <View style={styles.mosqueInfo}>
-                <View>
-                  <Text style={styles.mosqueName}>{item.name}</Text>
+            <TouchableHighlight
+              onPress={() => setCurrentMosque(item)}
+              underlayColor="#fcfcfc"
+            >
+              <View style={styles.mosqueItem}>
+                <View style={styles.mosqueIconContainer}>
+                  <Image
+                    style={styles.mosqueIcon}
+                    source={{ uri: item.imageUrl ? item.imageUrl : undefined }}
+                    defaultSource={{
+                      uri: "/Users/yaser/Projects/iqama-app/assets/mosque_placeholder.png",
+                    }}
+                  />
+                </View>
+                <View style={styles.mosqueInfo}>
+                  <View>
+                    <Text style={styles.mosqueName}>{item.name}</Text>
+                  </View>
+                </View>
+                <View style={styles.checkmarkIconContainer}>
+                  <View>
+                    <Image
+                      style={styles.checkmarkIcon}
+                      source={{
+                        uri:
+                          currentMosque == item
+                            ? "/Users/yaser/Projects/iqama-app/assets/checkmark.png"
+                            : undefined,
+                      }}
+                    />
+                  </View>
                 </View>
               </View>
-            </View>
+            </TouchableHighlight>
           )}
         />
       </View>
@@ -125,8 +230,18 @@ const styles = StyleSheet.create({
     height: null,
     resizeMode: "contain",
   },
-  imageContainer: {
+  mosqueIconContainer: {
     width: 50,
     height: 50,
+  },
+  checkmarkIconContainer: {
+    flex: 0.15,
+    justifyContent: "center",
+  },
+  checkmarkIcon: {
+    flex: 1,
+    width: 20,
+    height: 15,
+    resizeMode: "contain",
   },
 });
